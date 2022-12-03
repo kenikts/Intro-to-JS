ca3b1a4120a870361581de63bd309f945aad3314
Tselikina Anna <kenikts@github.com>
2022-12-03 16:45:29 +0200

    Initial commit

---
+++ Intro to JS/script.js
@@ -0,0 +1,11 @@
+alert("Hello my name is Arick!");
+document.write("Hello");
+document.write("Who are you?");
+var nickname = 'Arick';
+alert(nickname);
+
+// два варіанти коду - перший чисто з виправленням граматичних помилок, а другий з працюючим привітанням до користувача
+
+alert("Hello, my name is Arick!");
+var nickname = prompt('Who are you?');
+alert(`Hello, ${nickname}!`);
